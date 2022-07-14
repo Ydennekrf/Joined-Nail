3b6552ac09252b85ba4acc5046687e2d62ee66fa
ydennekrf <ydennekrf@gmail.com>
2022-07-14 08:50:14 -0400

    removed  from webpack that were not required

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -32,17 +32,8 @@ module.exports = () => {
         name: "Just Another Text Editor",
         short_name: "J.A.T.E",
         description: "Takes notes with JavaScript syntax highlighting!",
-        background_color: "#225ca3",
-        theme_color: "#225ca3",
         start_url: "/",
         publicPath: "/",
-        icons: [
-          {
-            src: path.resolve("src/images/logo.png"),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join("assets", "icons"),
-          },
-        ],
       }),
     ],
 
