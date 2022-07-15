85584d240ad25024cd16828409de29bd613e2f19
ydennekrf <ydennekrf@gmail.com>
2022-07-15 08:17:16 -0400

    added scripts, install event listeners and typos in database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,24 +1,24 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('data', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('data')) {
+        console.log('data database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('data', { keyPath: 'id', autoIncrement: true });
+      console.log('data database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+export const putDb = async (id, content) => {
   console.log('PUT to the db');
   const dataDb = await openDB('data', 1);
   const txt = dataDb.transaction('data', 'readwrite');
   const store = txt.objectStore('data');
-  const request = store.put({ id: 1, value: content});
+  const request = store.put({ id: id, value: content});
   const result = await request;
   console.log('🚀 - data has been updated', result.value);
 };
